957c846224158f5e6034b3bfd5592b80d7384a07
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2015-11-02 19:20:14 -0500

    Changed from files to socket to get more control over tests

--- src/main/scala/org/typesafe/spark/mesos/framework/runners/SparkJobRunner.scala
+++ src/main/scala/org/typesafe/spark/mesos/framework/runners/SparkJobRunner.scala
@@ -1,81 +1,33 @@
 package org.typesafe.spark.mesos.framework.runners
 
-import java.io.{PrintWriter, FileWriter}
-
-import org.apache.spark._
-import org.scalatest.events._
-import org.scalatest.{FunSuite, Reporter, Args}
-import org.typesafe.spark.mesos.tests.client.{ClientFineGrainMode, ClientCoarseGrainMode}
-import org.typesafe.spark.mesos.tests.cluster.{ClusterCoarseGrainMode, ClusterFineGrainMode}
+import java.io.{FileWriter, PrintWriter}
+import java.net.InetAddress
 
 import scala.collection.mutable.{Set => MSet}
 
+import org.apache.spark._
+import org.typesafe.spark.mesos.tests.{ClientModeSpec, ClusterModeSpec}
+
 case class TestResult(testName: String, isSuccess: Boolean, message: Option[String] = None)
-
-case class TestResultCollector(results: List[TestResult] = Nil) {
-  def printResult(logFile: String): Unit = {
-    //TODO: Change to new Java 8 classes.
-    val writer = new PrintWriter(new FileWriter(logFile))
-    results.foreach(line => writer.println(line))
-    writer.close()
-
-    val doneFile = new PrintWriter(new FileWriter(logFile + ".done"))
-    doneFile.close()
-
-  }
-
-  def addAll(r2: TestResultCollector): TestResultCollector = TestResultCollector(this.results ++ r2.results)
-
-  def add(t: TestResult): TestResultCollector = TestResultCollector(results :+ t)
-
-}
-
-object TestResultCollector {
-
-  implicit object TestResultAccumulatorParam extends AccumulableParam[TestResultCollector, TestResult] {
-    override def addAccumulator(r: TestResultCollector, t: TestResult): TestResultCollector = r.add(t)
-
-    override def addInPlace(r1: TestResultCollector, r2: TestResultCollector): TestResultCollector = r1.addAll(r2)
-
-    override def zero(initialValue: TestResultCollector): TestResultCollector = TestResultCollector()
-  }
-}
 
 object SparkJobRunner {
 
   def main(args: Array[String]): Unit = {
     val mesosConsoleUrl = args(0)
-    val mode = args(1)
-    val deployMode = args(2)
-    val logFile = args(3)
-
-    val sparkConf = new SparkConf()
-      .setAppName("Mesos integration test")
-      .set("spark.executor.memory", "512mb")
-
-    if(mode == "coarse-grained") {
-      sparkConf.set("spark.mesos.coarse", "true")
-    }
-    if(mode == "fine-grained") {
-      sparkConf.set("spark.mesos.coarse", "false")
-    }
-
-
-    val sc = new SparkContext(sparkConf)
-    val accumulable: Accumulable[TestResultCollector, TestResult] =
-      sc.accumulable[TestResultCollector, TestResult](new TestResultCollector())
+    val deployMode = args(1)
+    val runnerAddress = InetAddress.getByName(args(2))
 
     try {
-      val testToRun = (deployMode, mode) match {
-        case ("cluster", "fine-grained") => new ClusterFineGrainMode(sc, mesosConsoleUrl, accumulable)
-        case ("cluster", "coarse-grained") => new ClusterCoarseGrainMode(sc, mesosConsoleUrl, accumulable)
-        case ("client", "fine-grained") => new ClientFineGrainMode(sc, mesosConsoleUrl, accumulable)
-        case ("client", "coarse-grained") => new ClientCoarseGrainMode(sc, mesosConsoleUrl, accumulable)
+      val testToRun = deployMode match {
+        case "cluster" => new ClusterModeSpec(mesosConsoleUrl, runnerAddress)
+        case "client" => new ClientModeSpec(mesosConsoleUrl, runnerAddress)
       }
       org.scalatest.run(testToRun)
-      accumulable.value.printResult(logFile)
     } finally {
-      sc.stop()
+      notifyJobFinished(runnerAddress)
     }
   }
+
+  private def notifyJobFinished(runnerAddress: InetAddress): Unit = Utils.sendMessage(runnerAddress, "<DONE/>")
+
 }
